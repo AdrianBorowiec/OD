757425292dbcddef5112dda7cd7a8d885b8dbce1
AdrianBorowiec <adrian.borowiec94@gmail.com>
2016-12-27 17:45:46 +0100

    Dodanie dostępnej ilości produktów

--- OD/App_Start/BundleConfig.cs
+++ OD/App_Start/BundleConfig.cs
@@ -11,8 +11,14 @@ namespace OD
             bundles.Add(new ScriptBundle("~/bundles/jquery").Include(
                         "~/Scripts/jquery-{version}.js"));
 
+            bundles.Add(new ScriptBundle("~/bundles/jqueryUI").Include(
+                "~/Scripts/jquery-ui-{version}.js"));
+
             bundles.Add(new ScriptBundle("~/bundles/jqueryval").Include(
                         "~/Scripts/jquery.validate*"));
+
+            bundles.Add(new ScriptBundle("~/bundles/knockout").Include(
+                        "~/Scripts/knockout-{version}.js"));
 
             // Use the development version of Modernizr to develop with and learn from. Then, when you're
             // ready for production, use the build tool at http://modernizr.com to pick only the tests you need.
@@ -25,7 +31,7 @@ namespace OD
 
             bundles.Add(new StyleBundle("~/Content/css").Include(
                       "~/Content/bootstrap.css",
-                      "~/Content/site.css"));
+                      "~/Content/Site.css"));
         }
     }
 }
---
+++ OD/Controllers/ShoppingCartController.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace OD.Controllers
+{
+    public class ShoppingCartController : Controller
+    {
+        // GET: ShoppingCart
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult AddToCart(int id, int quantity)
+        {
+            return RedirectToAction("Index");
+        }
+    }
+}
