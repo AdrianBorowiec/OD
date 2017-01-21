bb41a2d28492bfa5458751f214c7953b4cf2d3ae
Aga <agnieszkaa.pastwa@gmail.com>
2017-01-21 17:36:24 +0100

    User Model

---
+++ OD/Configurations/UserConfiguration.cs
@@ -0,0 +1,22 @@
+﻿using OD.Models;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity.ModelConfiguration;
+using System.Linq;
+using System.Web;
+
+namespace OD.Configurations
+{
+    public class UserConfiguration : EntityTypeConfiguration<User>
+    {
+        public UserConfiguration()
+        {
+            ToTable("User");
+
+            Property(x => x.Id).IsRequired();
+            Property(x => x.Name).IsRequired();
+            Property(x => x.Password).IsRequired();
+            Property(x => x.Email).IsOptional();
+        }
+    }
+}
---
+++ OD/Validators/UserValidator.cs
@@ -0,0 +1,21 @@
+﻿using FluentValidation;
+using OD.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace OD.Validators
+{
+    public class UserValidator : AbstractValidator<User>
+    {
+
+        public UserValidator()
+        {
+            RuleFor(x => x.Id).NotEmpty();
+            RuleFor(x => x.Name).NotEmpty();
+            RuleFor(x => x.Password).NotEmpty();
+            RuleFor(x => x.Email).EmailAddress();
+        }
+    }
+}
